40fb63880713869c444978b1542b773d824e224d
Lucas <lucass.ambar@gmail.com>
2022-11-15 17:23:40 -0300

    feat: post route wallet

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import dotenv from "dotenv";
 import joi from 'joi';
 import bcrypt from 'bcrypt';
 import { v4 as uuid } from 'uuid';
+import dayjs from 'dayjs';
 
 //config
 dotenv.config();
@@ -33,7 +34,12 @@ const userSchema = joi.object({
     "password": joi.string().required().min(3).alphanum(),
     "confirmePassword": joi.ref("password")
 })
-
+const walletSchema = joi.object({
+    "type": joi.string().valid("entrance","exit").required(),
+    "amount": joi.number().precision(2).required(),
+    "description": joi.string().min(5).required() 
+})
+//routes
 app.post("/sign-up", async (req,res) => {
     const user = req.body
 
@@ -92,5 +98,58 @@ app.post("/sign-in", async (req, res) => {
 
     }catch (erro) {res.sendStatus(500)}
 })
+
+app.post("/wallet", async (req, res) => {
+    const {authorization} = req.headers
+    const token =  authorization?.replace('Bearer ', '');
+
+    if (!token) {res.sendStatus(401);return}
+
+    const session = await collectionSessions.findOne({token})
+    if (!session) {res.sendStatus(401); return}
+
+    const validation = walletSchema.validate(req.body, {abortEarly: false})
+    if (validation.error) {
+        const errors = validation.error.details.map((detail) => detail.message);
+        res.status(422).send(errors);
+        return;
+    }
+
+    const {type, amount, description} = req.body
+    const wallet = {
+        userId: session.userId,
+        type,
+        amount,
+        description,
+        date: dayjs().format("DD/MM")
+    }
+
+    await collectionWallet.insertOne(wallet)
+
+    let balanceDB = await collectionBalance.findOne({userId: session.userId})
+
+    if (!balanceDB) {
+        balanceDB = {
+            userId: session.userId,
+            balance: 0
+        }
+        await collectionBalance.insertOne(balanceDB)
+    }
+
+    let balance;
+    switch (type) {
+        case ("exit") :
+            balance = balanceDB.balance - amount
+            await collectionBalance.updateOne({userId: session.userId}, { $set: {balance: balance} })
+            break
+        case ("entrance") :
+            balance = Number(balanceDB.balance) + Number(amount)
+            await collectionBalance.updateOne({userId: session.userId}, { $set: {balance: balance} })
+            break
+    }
+
+    res.send(balanceDB)
+})
+
 
 app.listen(process.env.PORT, () => console.log(`Server running in port: ${process.env.PORT}`))
